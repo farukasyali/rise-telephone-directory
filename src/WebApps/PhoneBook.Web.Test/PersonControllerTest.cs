dc2ea83194b289ec68159d01d18206f7010123e3
Ömer Faruk Asyalıoğlu <faruk.asyalioglu@sbu.edu.tr>
2021-08-23 12:13:03 +0300

    PhoneBook.Web.Test projesi eklendi.

---
+++ src/WebApps/PhoneBook.Web.Test/PersonControllerTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using PhoneBook.Web.Controllers;
+using PhoneBook.Web.Services.Abstract;
+using Xunit;
+
+namespace PhoneBook.Web.Test
+{
+    public class PersonControllerTest
+    {
+        private readonly Mock<IPersonService> _mockRepo;
+
+        private readonly PersonController _controller;
+
+        public PersonControllerTest()
+        {
+            _mockRepo = new Mock<IPersonService>();
+            _controller = new PersonController(_mockRepo.Object);
+        }
+
+        [Fact]
+        public void Index_ActionExecute_ReturnView()
+        {
+            var result = _controller.Index();
+
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
